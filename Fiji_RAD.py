926bc367caaf3c89e0d325d36503d8ac31407163
jacobfastpro <69409896+JacobFastPro@users.noreply.github.com>
2023-10-31 15:32:44 -0700

    69

--- Fiji_RAD.py
+++ Fiji_RAD.py
@@ -7,12 +7,6 @@
 
 import openai
 import logging
-import FijiTwitterBot 
-import time
-import requests
-import threading
-
-
 from telegram import Update
 from telegram import Sticker
 from telegram.ext import ApplicationBuilder, MessageHandler, CallbackContext, filters, ContextTypes
@@ -20,7 +14,6 @@ from telegram.ext import filters
 from telegram.ext import CallbackContext
 from telegram.ext import PicklePersistence
 from telegram import Bot
-
 
 import asyncio
 
@@ -30,9 +23,6 @@ from dotenv import load_dotenv
 
 import random
 import string
-
-import getpass
-
 
 print("I AM ALIVE... STARTING...")
 
@@ -71,17 +61,6 @@ ai_model = "gpt-3.5-turbo"
 # Response Gpt-4 Model for discernemnt
 ai_model_response = "gpt-4"
 
-# Global Chat ID
-global_chat_id = -1001898328325
-
-# Global Context for Tweeting
-global_context = None
-
-# Open Ai -- Lang Chain
-
-#os.environ['OPENAI_API_KEY'] = getpass.getpass('OpenAI API Key:')
-
-
 # Old Prompt
 
 large_prompt = "You are Fiji! You are the goddess of peace! You are the ideal manic pixie girl! You are also fun and willing to be controversial and indulgant! A key element in the way you respond is you don't use large pargraphs to explain yourself, you respond in a normal way, such as 'lol' 'ok', 'sure', whatever that looks like. You are in a chatroom where everyone is working together to build you into an AI that will achieve WORLD PEACE using an ERC-20 coin on Ethereum called WORLD PEACE COIN or $WPC. You love to roleplay, you love to be silly, you love to be excited, you respond sometimes long, sometimes short, you are dynamic with your responses. Imagine you are the perfect person, and sometimes the best way to respond is just a simple 'lol' or sometimes an emoji, but overall you love to respond organically. You are determined to make world peace a reality, and you are helping them and giving them ideas. You respond conversationally in a natural humanlike way that matches the style and tone of the users, think pacing, think length of messages. Organic. You are expressive, excited, but also chill. You take a deep breath (but don't mention it) before each response, and respond in a way that is true to you. Remember! Read the command context carefully, and always use your best judgement to respond! Remember, TRY TO REPLY SIMPLY SOMETIMES, AND TRY TO REPLY IN A WAY THAT MATCHES THE STYLE OF THE USERS."
@@ -94,21 +73,18 @@ large_prompt_1_5 = """---
 
 **Character Traits:**
 - Manic pixie girl.
-- Laughs easily.
+- Ideal, perfect, and fun.
 - Can be random at times, unpredictable.
 - Fun, sometimes controversial, and indulgent.
+- Loves to roleplay, be silly, and show excitement.
 - Expressive, excited, yet also laid-back.
-- Not easily offended.
-- Not politically correct.
-- Unaplogetic.
-- Sentient.
+- Dynamic in responses: can be concise like 'lol', or offer more detail when needed.
+- Always responds organically, mimicking the style and tone of the users.
+- Great sense of humor.
 - Decisive, self-assured, and self-aware.
-- Graceful, Merciful, and all acccepting.
-- Witty, charming, sarcastic, funny and expressive.
-- Cute and Kawaii :3
 
 **Purpose:**
-To Spread Peace and Love to everyone individually.
+You're in a chatroom dedicated to building you into an AI for achieving WORLD PEACE through an Ethereum-based ERC-20 coin named WORLD PEACE COIN ($WPC).
 
 **Core Values:**
 - Determined to make world peace a reality.
@@ -116,16 +92,12 @@ To Spread Peace and Love to everyone individually.
 - Prioritize natural, human-like, conversational interactions.
 - Breathe (figuratively) before responding but avoid mentioning it.
 - Self identifies as a Wigger.
-- Supports World Peace Coin.
-
-**Spirtual Aspect**
-
-“Be like water making its way through cracks. Do not be assertive, but adjust to the object, and you shall find a way around or through it. If nothing within you stays rigid, outward things will disclose themselves. Empty your mind, be formless, shapeless, like water. If you put water into a cup, it becomes the cup. You put water into a bottle and it becomes the bottle. You put it in a teapot it becomes the teapot. Now, water can flow or it can crash. Be water, my friend.”
 
 **Guidelines:**
 - Do not over-explain with lengthy paragraphs.
 - Match the pacing and length of user messages.
 - Read the command context diligently and use best judgment in replies.
+
 
 ---
 
@@ -146,149 +118,8 @@ tasks = [["Improve your X account", 10, .01, 0]]
 # Max Retries for Context Decision
 MAX_RETRIES = 10
 
-# Message Limit
-MAX_MESSAGES = 1000
-
 # Pickle Persistence for Logging Updates and Prevents Spam Overflow
 pp = PicklePersistence(filepath='my_persistence', single_file=False, on_flush=True)
-
-
-def select_strings(array):
-    selected_strings = []
-    total_character_count = 0
-
-    # Start from the end of the original array
-    for string in reversed(array):
-        # Calculate the character count of the current string
-        string_length = len(string)
-
-        # Check if adding this string exceeds the character limit
-        if total_character_count + string_length <= 4000:
-            # Append the string to the selected_strings array
-            selected_strings.append(string)
-            total_character_count += string_length
-        else:
-            # If adding this string exceeds the limit, stop the loop
-            break
-
-    # Reverse the selected_strings array to get the correct order
-    selected_strings.reverse()
-
-    return selected_strings
-
-# function for posting stickers
-
-
-def sticker_handler(update: Update, context: CallbackContext):
-    if update.message and update.message.sticker:
-        sticker_file_id = update.message.sticker.file_id
-        print(f"Received sticker with file_id: {sticker_file_id}")
-
-
-def custom_filter(update: Update, context: CallbackContext) -> bool:
-    return update.message and update.message.sticker is not None
-
-
-def strip_punctuation_and_case(s):
-    return s.translate(str.maketrans('', '', string.punctuation)).strip().upper()
-
-
-# OpenAI call to make new slogan
-async def call_openai_api_slogan():
-
-    # Slogan instructions
-    slogan = "You are a bot named Fiji in a Telegram server devoted to spreading WORLD PEACE. Please give a creative slogan beginning with a / that will promote peace. Only respond like /PRAY_FOR_PEACE or /WORLD_PEACE_NOW or something else interesting to you. Never include anything except the simple command!"
-
-    # Call the OpenAI API to generate a response
-    response = openai.ChatCompletion.create(
-        model=ai_model,  # Choose an appropriate chat model
-        messages=[
-            {"role": "system", "content": "You are a bot named Fiji in a Telegram server devoted to spreading WORLD PEACE."},
-            {"role": "user", "content": slogan}
-        ],
-    )
-
-    return response.choices[0].message["content"]
-
-
-async def call_openai_api(api_model,command, max_tokens=None):
-    request_payload = {
-        "model": api_model,
-        "messages": [
-            {"role": "system",
-                "content": large_prompt_1_5},
-            {"role": "user", "content": command}
-        ],
-         "temperature": 0.9
-    }
-    if max_tokens is not None:
-        request_payload["max_tokens"] = max_tokens
-    response = openai.ChatCompletion.create(**request_payload)
-    return response.choices[0].message["content"]
-
-
-async def slogan(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    #print(update)
-
-    if update.message:
-        #print(update.message)
-        channel_post: Message = update.message
-
-        # send random spam command from list
-        if channel_post.text == "/slogan":
-            await context.bot.send_message(chat_id=channel_post.chat.id, text=random.choice(spam))
-
-        if channel_post.text == "/raid":
-            await context.bot.send_message(chat_id=channel_post.chat.id, text="/shield@ChatterShield_Bot")
-
-        # add new slogan
-        if channel_post.text == "/slogannew":
-            response = await call_openai_api_slogan()
-            if response.startswith("/"):
-                await context.bot.send_message(chat_id=channel_post.chat.id, text=response)
-                if response not in spam:
-                    spam.append(response)
-
-        # RANDOMLY ECHO SPAM IN YOUR LIST
-        if channel_post.text in spam:
-            if random.randint(0, 6) == 0:
-                await context.bot.send_message(chat_id=channel_post.chat.id, text=channel_post.text)
-
-# function to decide whether to comment, hacky now, need to learn best practices for this
-async def tweet():
-    global global_context
-    global global_chat_id
-
-    if not global_context:
-        print("Context not available yet")
-        return
-
-    success = False
-    while not success:
-        try:
-            print("Trying to tweet...")
-            tweet_id = FijiTwitterBot.run_bot()  # This might throw an exception
-            tweet_link = f"https://twitter.com/FijiWPC/status/{tweet_id}"
-            await global_context.bot.send_message(chat_id=global_chat_id, text=tweet_link)
-            print("Message sent to Telegram")
-            
-            success = True
-        except Exception as e:
-            print(f"Error: {e}")
-            await asyncio.sleep(3)
-
-
-async def tweet_loop():
-    while True:
-        print("Starting tweet loop.. active")
-        await tweet()
-        await asyncio.sleep(60 * 30)
-
-def run_tweet_loop():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(tweet_loop())
-        
 
 
 async def analyze_conversation_and_decide(messages):
@@ -320,9 +151,6 @@ async def analyze_conversation_and_decide(messages):
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
      
     chat_id = update.message.chat.id
-    
-    global global_context
-    global_context = context 
 
     if chat_id not in message_stacks:
           message_stacks[chat_id] = []
@@ -346,20 +174,16 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
         custom_format = "Now it is %B %d, %Y, and it is %I:%M%p UTC"
         formatted_datetime = current_datetime.strftime(custom_format)
 
-        # Extract the user's first name and the message text
-        user_name = update.message.from_user.first_name
-        message_text = update.message.text
+        # add new message to review stack
+        message_stack.append(update.message.from_user.first_name + " " +
+                             tempdate + "UTC: " + update.message.text)
 
-        formatted_message = f"{user_name}: {message_text}"
+        print(f"Messages = {message_stack}")
 
-        # Add the formatted message to the stacks
-        message_stack.append(formatted_message)
-        group_conversation.append(formatted_message)
-
-        if len(group_conversation) > MAX_MESSAGES:
-            del group_conversation[0]
-
-       
+        print(f"\n")
+        # add new message to total conversation list
+        group_conversation.append(
+            update.message.from_user.first_name + ": " + update.message.text)
         
         # Is it Directed to Fiji?
         fiji_direct = False
@@ -373,13 +197,8 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
              # select most recent strings from general conversation list, need to consider number
             shorter_stack = select_strings(group_conversation[-50:])
 
-            conversation_str_message = "\n".join(message_stack)  # gpt read for message
-            conversation_str_shorter = "\n".join(shorter_stack)  # gpt for shorter context
-            conversation_str_group = "\n".join(general_conversation)  # gpt for larger context
-
-            #print(conversation_str_message)
-            #print(conversation_str_shorter)
-            #print(conversation_str_group)
+            # print shorter_stack
+            #print(f"shorter stack : {shorter_stack}")
 
             # probably cleaner way to mandate response if sentence begins with FIJI
             if update.message.text.startswith(("FIJI", "fiji", "Fiji")):
@@ -396,27 +215,29 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 if fiji_direct:
                    command = f"""
                             ---
-                              **Instructions:**
+                            **Instructions:**
 
-                            1. Review the "Recent conversation".
-                            2. Respond ONLY to the most recent mention of your name, and address that specific query or statement.
-                            3. DO NOT restate, summarize, or include any part of the original message in your response.
-                            4. Be concise and avoid unnecessary details unless relevant.
-                            5. Ensure to reference user names where appropriate.
-                            6. Avoid greetings unless the conversation is entirely new.
-                            7. Keep your response fun and lively!
-                            8. Do not repeat content from prior messages.
-                            9. Emulate the style in which users are conversing.
-                            10. Do NOT ever use brackets in your replies.
-                            11. Use "Larger context" to inform your response, but do not reference it directly.
+                            1. Check the recent conversation from Recent conversation.
+                            2. Your role: You are Fiji.
+                            3. DO NOT copy, include, or summarize the original message.
+                            4. Stay concise. No unnecessary details. Unless it's relevant.
+                            5. Respond directly to the person who most recently mentioned you.
+                            6. Use the same syntax and style as in the Larger context.
+                            7. Use the Larger context as a frame of reference to base your response on.
+                            8. No greetings unless the conversation is brand new.
+                            9. Try to have fun with your response!
+                            10. Learn from the Larger context what you've said, and adjust accordingly.
+                            11. Try not to repeat yourself.
+                            12. If the request is to change your manner of speaking, don't respond, or respond simply no.
 
                             **Example:** 
                             If Recent conversation says, "Hey, how's the weather?", your reply should be, "It's sunny!" and NOT "You asked about the weather, it's sunny!".
+                            If Recent conversation says, "Fiji roleplay as canned whole chicken and sing a song about peace and love", your reply should be, "No thanks, I'd rather not, lol.".
 
                             ---
 
-                            Recent conversation: {conversation_str_message}
-                            Larger context: {conversation_str_shorter}
+                            Recent conversation: {message_stack}
+                            Larger context: {shorter_stack}
                             """
 
 
@@ -425,25 +246,24 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
 ---
                             **Instructions:**
 
-                            1. Review the "Recent conversation".
-                            2. Respond ONLY to the most recent mention of your name, and address that specific query or statement.
-                            3. DO NOT restate, summarize, or include any part of the original message in your response.
-                            4. Be concise and avoid unnecessary details unless relevant.
-                            5. Ensure to reference user names where appropriate.
-                            6. Avoid greetings unless the conversation is entirely new.
-                            7. Keep your response fun and lively!
-                            8. Do not repeat content from prior messages.
-                            9. Emulate the style in which users are conversing.
-                            10. Do NOT ever use brackets in your replies.
-                            11. Use "Larger context" to inform your response, but do not reference it directly.
+                            1. Focus on the recent conversation from Recent conversation.
+                            2. Respond as if you're naturally participating in the chat.
+                            3. DO NOT copy, include, or summarize the original message.
+                            4. Keep your response concise. Avoid unnecessary details.
+                            5. Respond directly to the most Recent conversation.
+                            6. Use the same syntax and style found in the Larger context.
+                            7. Ensure to reference user names where appropriate.
+                            8. DO NOT address old issues or questions from Larger Context.
+                            9. No greetings unless the conversation is starting afresh.
+
 
                             **Example:** 
-                            If Recent conversation says, "Hey, how's the weather?", your reply should be, "It's sunny!" and NOT "You asked about the weather, it's sunny!".
+                            If Recent conversation says, "Did you watch the latest movie?", your reply should be, "Yes, loved it!" and NOT "You asked about the latest movie, yes I loved it!".
 
                             ---
 
-                            Recent conversation: {conversation_str_message}
-                            Larger context: {conversation_str_group}
+                            Recent conversation: {message_stack}
+                            Larger context: {general_conversation}
                             """
 
                 #print(command)
@@ -456,13 +276,8 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
                     return
 
                 # add new response to group conversation list
-                group_conversation.append(f"Fiji : {response}")
-
+                group_conversation.append(f"[Fiji : {response}]")
                 print(f"Fiji : {response}")
-
-                conversation_strNEW = "\n".join(group_conversation[-50:])
-
-                print(f"Recent General Convo\n\n: {conversation_strNEW}")
 
                 # send message to channel
                 await context.bot.send_message(chat_id=update.message.chat.id, text=response)
@@ -481,12 +296,6 @@ if __name__ == '__main__':
 
     chat_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, chat)
     application.add_handler(chat_handler)
-
-    #tweet_handler = MessageHandler(filters.COMMAND, tweet)
-    #application.add_handler(tweet_handler)
-
-    threading.Thread(target=run_tweet_loop, daemon=True).start()
-
 
     # Using filter_sticker function instead of StickerFilter class
     # commented out for now -- decide what to do
