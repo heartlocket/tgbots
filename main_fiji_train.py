26238f9eb4a7cafafead1b0570b8fea60c412e36
jacobfastpro <69409896+JacobFastPro@users.noreply.github.com>
2023-11-25 04:29:15 -0800

    test3

--- main_fiji_train.py
+++ main_fiji_train.py
@@ -3,7 +3,7 @@
 #   / /_   / /__  / // /
 #  / __/ _/ // /_/ // /
 # /_/   /___/\____/___/
-# TELEGRAM CHATBOT FOR WORLD PEACE, VERSION 0.33
+# TELEGRAM CHATBOT FOR WORLD PEACE, VERSION 0.05
 
 #HELLO WORLD HEART GUY
 
